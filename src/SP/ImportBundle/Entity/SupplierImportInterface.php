305b43077a0d0d66e93c4d29353a13128b90c2c0
Claire REMY <anha.flint@gmail.com>
2015-06-02 17:11:03 +0100

    Add SupplierImportInterface

---
+++ src/SP/ImportBundle/Entity/SupplierImportInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SP\ImportBundle\Entity;
+
+
+/**
+ * SupplierImportInterface
+ *
+ */
+Interface SupplierImportInterface
+{
+    public function getAllVenues();
+
+    public function getAllProductions();
+}

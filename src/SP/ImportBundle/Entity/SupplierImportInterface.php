df4b488a022a7858f3c8812b06414b9fb3b39a19
Claire REMY <anha.flint@gmail.com>
2015-06-03 14:02:52 +0100

    Update methodes name

--- src/SP/ImportBundle/Entity/SupplierImportInterface.php
+++ src/SP/ImportBundle/Entity/SupplierImportInterface.php
@@ -9,7 +9,21 @@ namespace SP\ImportBundle\Entity;
  */
 Interface SupplierImportInterface
 {
-    public function getAllVenues();
+    /**
+     * Returns all venues for the supplier
+     * Or just one if $id is defined
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function getVenues($id);
 
-    public function getAllProductions();
+    /**
+     * Returns all productions for the supplier
+     * Or just one production if $id is defined
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function getProductions($id);
 }
